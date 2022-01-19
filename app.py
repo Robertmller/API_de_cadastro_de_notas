9111e8d75e6667c24dce3275a0d19c4c64cfae60
Robertmller <contatorobertmull@gmail.com>
2022-01-19 14:36:07 -0300

    Salvando de forma continua no Banco

--- app.py
+++ app.py
@@ -93,8 +93,46 @@ class Professor(db.Model):
     turma = db.Column(db.String)
 
 
+# Limpa estrutura do Banco de Dados
 db.drop_all()
+# Cria a estrutura nova
 db.create_all()
+
+##########
+
+
+def mediax(a, b, c, d):
+    return (a + b + c + d) / 4
+
+
+while True:  # loop infinito
+    nome = input("Nome do aluno: ")
+    turma = input("informe a turma: ")
+    nota1 = float(input('entre com a primeira nota: '))
+    nota2 = float(input('entre com a segunda nota: '))
+    nota3 = float(input('entre com a terceira nota: '))
+    nota4 = float(input('entre com a quarta nota: '))
+    media = mediax(nota1, nota2, nota3, nota4)
+
+    aluno = Aluno(nome=nome, turma=turma, nota1=nota1,
+                  nota2=nota2, nota3=nota3, nota4=nota4, media=media)
+
+    db.session.add(aluno)
+    db.session.commit()
+
+    print(media)
+
+    if media < 4:
+        print('aluno reprovado')
+    elif media < 6 > 4:
+        print('aluno em recuperação')
+    else:
+        print("aluno aprovado")
+
+    if input('para sair digite (fim) para continuar (enter) ') == 'fim':
+        break  # se digitar "fim", sai do while True
+
+#########
 
 
 app.run(port=5000, host='localhost', debug=True)
